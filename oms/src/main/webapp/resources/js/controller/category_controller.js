eea0f30e2e2196b76e64073774d5f6cca89abce2
Mahesh Dontula <mahesh.dontula@macys.com>
2015-10-20 22:53:57 +0530

    Added code for edit

--- oms/src/main/webapp/resources/js/controller/category_controller.js
+++ oms/src/main/webapp/resources/js/controller/category_controller.js
@@ -17,8 +17,8 @@ omsApp.controller('CategoryController', ['$scope', 'CategoryService','$uibModal'
           };
 
           $scope.fetchAllCategories();
-          
-          
+
+
           $scope.addCategory = function($scopecategory){
               CategoryService.addCategory($scope.category)
                   .then(
@@ -31,7 +31,7 @@ omsApp.controller('CategoryController', ['$scope', 'CategoryService','$uibModal'
       			       );
           };
 
-          $scope.editCategoryModal = function (size, selectedCategory) {
+          /*$scope.editCategoryModal = function (size, selectedCategory) {
 
         	    var modalInstance = $uibModal.open({
         	      animation: $scope.animationsEnabled,
@@ -52,12 +52,57 @@ omsApp.controller('CategoryController', ['$scope', 'CategoryService','$uibModal'
         	    }, function () {
         	      $log.info('Modal dismissed at: ' + new Date());
         	    });
-        	  };
+        	  };*/
 
-          
+						$scope.editCategoryModal = function (category) {
+							$scope.showModal = true;
+							$scope.category = category;
+						};
 
          }]);
 
+
+				 omsApp.directive('modal', function () {
+				     return {
+				       template: '<div class="modal fade">' +
+				           '<div class="modal-dialog">' +
+				             '<div class="modal-content">' +
+				               '<div class="modal-header">' +
+				                 '<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>' +
+				                 '<h4 class="modal-title">{{ title }}</h4>' +
+				               '</div>' +
+				               '<div class="modal-body" ng-transclude></div>' +
+				             '</div>' +
+				           '</div>' +
+				         '</div>',
+				       restrict: 'E',
+				       transclude: true,
+				       replace:true,
+				       scope:true,
+				       link: function postLink(scope, element, attrs) {
+				         scope.title = attrs.title;
+
+				         scope.$watch(attrs.visible, function(value){
+				           if(value == true)
+				             $(element).modal('show');
+				           else
+				             $(element).modal('hide');
+				         });
+
+				         $(element).on('shown.bs.modal', function(){
+				           scope.$apply(function(){
+				             scope.$parent[attrs.visible] = true;
+				           });
+				         });
+
+				         $(element).on('hidden.bs.modal', function(){
+				           scope.$apply(function(){
+				             scope.$parent[attrs.visible] = false;
+				           });
+				         });
+				       }
+				     };
+				   });
 /*$(function () {
   $('#categoryTable').DataTable({
     "paging": true,
